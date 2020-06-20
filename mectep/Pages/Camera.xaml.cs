34503f3c1d14f6e0aed79a9384b79759403f92cf
maksatbek1998 <maksat.140298.kg@gmail.com>
2020-06-20 10:45:52 +0600

    Maksatbek kommit

---
+++ mectep/Pages/Camera.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace mectep.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Camera.xaml
+    /// </summary>
+    public partial class Camera : Page
+    {
+        public Camera()
+        {
+            InitializeComponent();
+        }
+
+        private void Camera_Jandyruu_Click(object sender, RoutedEventArgs e)
+        {
+            if (Camera_Jandyruu.IsChecked==true)
+            {
+                Camera_Message.Text = "Баарын өчүрүү";
+            }
+            else
+            {
+                Camera_Message.Text = "Баарын жандыруу";
+            }
+        }
+    }
+}
